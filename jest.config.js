c5b6c2a028a5d41043709b8f37195795cdf8ffe3
piotrbod <peter.bod@gmail.com>
2026-03-05 16:51:53 +0000

    fix: update Jest config to modern ts-jest syntax

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,13 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  globals: {
-    'ts-jest': {
+    '^.+\\.tsx?$': ['ts-jest', {
       tsconfig: {
         types: ['jest', 'node']
       }
-    }
+    }]
   }
 };
